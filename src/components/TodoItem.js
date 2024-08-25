d9770c8acc7cef140d5a704ba948507c54ca35e8
Adel <adelnmo2003@gmail.com>
2024-08-25 17:10:25 +0330

    implement progress bar

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,6 +11,7 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Chip from "@mui/material/Chip";
+import LinearProgress from "@mui/material/LinearProgress";
 
 // Icons
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -29,69 +30,83 @@ function TodoItem({ todo }) {
     timeStatus === "today"
       ? "Today"
       : `${Math.abs(Number(distance))} days ${timeStatus}`;
-  const timeLeft = 24 - dayjs().hour();
+  const timeLeft = 24 - dayjs().hour() + (dayjs().minute() * 5) / 300;
 
   return (
-    <Card
-      sx={{
-        display: "flex",
-        flexDirection: "row-reverse",
-        justifyContent: "space-between",
-      }}
-    >
-      <CardMedia
-        sx={{ width: "12rem" }}
-        image={todo.imgUrl}
-        title="green iguana"
-      />
-      <Box>
-        <CardContent>
-          <IconButton onClick={() => dispatch(toggleCompleted(todo.id))}>
-            {todo.completed ? (
-              <CheckCircleIcon fontSize="large" color="success" />
-            ) : (
-              <CheckCircleOutlineIcon fontSize="large" />
-            )}
-          </IconButton>
+    <Card>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row-reverse",
+          justifyContent: "space-between",
+        }}
+      >
+        <CardMedia
+          sx={{ width: "12rem" }}
+          image={todo.imgUrl}
+          title="green iguana"
+        />
+        <Box>
+          <CardContent>
+            <IconButton onClick={() => dispatch(toggleCompleted(todo.id))}>
+              {todo.completed ? (
+                <CheckCircleIcon fontSize="large" color="success" />
+              ) : (
+                <CheckCircleOutlineIcon fontSize="large" />
+              )}
+            </IconButton>
 
-          <Typography gutterBottom variant="h5" component="div">
-            {todo.title}
-          </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {todo.title}
+            </Typography>
 
-          <Typography variant="body2" color="text.secondary">
-            {todo.details}
-          </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {todo.details}
+            </Typography>
 
-          <Chip
-            label={string}
-            variant="filled"
-            color={
-              (timeStatus === "today" && timeLeft >= 14 && "success") ||
-              (timeStatus === "today" && timeLeft >= 4 && "warning") ||
-              (timeStatus === "today" && "error") ||
-              (timeStatus === "future" && "info") ||
-              (timeStatus === "past" && "default")
-            }
-            sx={{ mt: 2 }}
-          />
-        </CardContent>
+            <Chip
+              label={string}
+              variant="filled"
+              color={
+                (timeStatus === "today" && timeLeft >= 14 && "success") ||
+                (timeStatus === "today" && timeLeft >= 4 && "warning") ||
+                (timeStatus === "today" && "error") ||
+                (timeStatus === "future" && "info") ||
+                (timeStatus === "past" && "default")
+              }
+              sx={{ mt: 2 }}
+            />
+          </CardContent>
 
-        <CardActions>
-          <IconButton
-            color="error"
-            onClick={() => dispatch(deleteTodo(todo.id))}
-          >
-            <DeleteIcon />
-          </IconButton>
+          <CardActions>
+            <IconButton
+              color="error"
+              onClick={() => dispatch(deleteTodo(todo.id))}
+            >
+              <DeleteIcon />
+            </IconButton>
 
-          <IconButton
-            color="primary"
-            onClick={() => navigate(`/edit/${todo.id}`)}
-          >
-            <EditIcon />
-          </IconButton>
-        </CardActions>
+            <IconButton
+              color="primary"
+              onClick={() => navigate(`/edit/${todo.id}`)}
+            >
+              <EditIcon />
+            </IconButton>
+          </CardActions>
+        </Box>
       </Box>
+      {timeStatus === "today" && (
+        <LinearProgress
+          variant="determinate"
+          value={100 - (timeLeft * 100) / 24}
+          color={
+            (timeStatus === "today" && timeLeft >= 14 && "success") ||
+            (timeStatus === "today" && timeLeft >= 4 && "warning") ||
+            (timeStatus === "today" && "error")
+          }
+          sx={{ height: 6 }}
+        />
+      )}
     </Card>
   );
 }
