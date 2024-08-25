d2fbda49a6d5b44d7521a7b8b685dc4ff321e3c2
Adel <adelnmo2003@gmail.com>
2024-08-25 16:49:14 +0330

    implement time label

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { deleteTodo, toggleCompleted } from "../todoSlice";
+import dayjs from "dayjs";
 
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -9,6 +10,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
+import Chip from "@mui/material/Chip";
 
 // Icons
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -19,6 +21,15 @@ import EditIcon from "@mui/icons-material/Edit";
 function TodoItem({ todo }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const distance = dayjs(todo.deadLine).diff(dayjs(), "day");
+  const timeStatus =
+    distance === 0 ? "today" : distance < 0 ? "past" : "future";
+  const string =
+    timeStatus === "today"
+      ? "Today"
+      : `${Math.abs(Number(distance))} days ${timeStatus}`;
+  const timeLeft = 24 - dayjs().hour();
 
   return (
     <Card
@@ -50,6 +61,19 @@ function TodoItem({ todo }) {
           <Typography variant="body2" color="text.secondary">
             {todo.details}
           </Typography>
+
+          <Chip
+            label={string}
+            variant="filled"
+            color={
+              (timeStatus === "today" && timeLeft >= 14 && "success") ||
+              (timeStatus === "today" && timeLeft >= 4 && "warning") ||
+              (timeStatus === "today" && "error") ||
+              (timeStatus === "future" && "info") ||
+              (timeStatus === "past" && "default")
+            }
+            sx={{ mt: 2 }}
+          />
         </CardContent>
 
         <CardActions>
